0273484effa069811dbf7266148695fd7593fbda
Dhiego Ramos Pinto <dhgrp@tecgraf.puc-rio.br>
2020-10-11 12:41:28 -0300

    BeerRepository implementation

---
+++ src/main/java/com/github/dhiegorp/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.github.dhiegorp.beerservice.repositories;
+
+import com.github.dhiegorp.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
